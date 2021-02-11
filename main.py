f47e1e0f7065bceefcc32fa59222dbeb578d85f7
Leandro Castro Alves <leandrocastroalves25@outlook.com>
2021-02-11 17:44:09 -0300

    ''

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from localizacao import regiao, uf, munic
 from normalizacao import categ_adm_ies, org_ac_ies, ies, grupo_curso, curso
 from aluno import tabela_aluno
-from web_scraping import carrega_dados_bruto, extrai_link_zip, verifica_arquivo_diretorio
+from web_scraping_link_enade import carrega_dados_bruto, extrai_link_zip, verifica_arquivo_diretorio
 
 carrega_dados_bruto()
 extrai_link_zip()
